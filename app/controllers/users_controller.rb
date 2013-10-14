5dd3682ccea0c2c9b08a40bc3a8b13beb1366719
sheg <danforkosh@yahoo.com>
2013-10-13 20:41:35 -0400

    fix typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome Sheg's App!"
+      flash[:success] = "Welcome to Sheg's App!"
       redirect_to @user
     else
       render 'new'
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
