80ef48825733b87241b93e6f0d1dcae00a9426e6
sheg <danforkosh@yahoo.com>
2013-10-20 21:48:21 -0400

    another fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   def destroy
     user = User.find(params[:id])
     User.find(params[:id]).destroy
-    flash[:success] = "#{user} has successfully been deleted"
+    flash[:success] = "#{user.name} has successfully been deleted"
     redirect_to users_url
   end
 
